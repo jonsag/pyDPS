144fc4e7f2888990f84f70c6242934fb5183b015
jon <jonsagebrand@gmail.com>
2019-10-01 15:34:42 +0200

    new help

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pyDPS
-Connect to RDTech DPS buck converters, using rdserialtool
+Connect to RDTech DPS buck converters, using rdserialtool  
+
+Thanks to Ryan Finnie for rdserialtool, found at https://github.com/rfinnie/rdserialtool  
+
+Install rdserialtool
+==========
+Install prerequisites:  
+
+In ubuntu  
+
+>$ sudo apt install python3-serial  
+
+Via pip  
+>$ pip3 install pyserial  
+
+rdserialtool  
+>$ git clone https://github.com/rfinnie/rdserialtool.git  
+>$ cd rdserialtool  
+>$ sudo python3 setup.py install  
+
--- modules.py
+++ modules.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Encoding: UTF-8
 
-import configparser, os, sys
+import configparser, os, sys, subprocess
 
 config = configparser.ConfigParser()  # define config file
 config.read("%s/config.ini" % os.path.dirname(os.path.realpath(__file__)))  # read config file
@@ -13,7 +13,7 @@ var = config.get('header', 'var').strip()
 # handle errors
 def onError(errorCode, extra):
     print("\nError:")
-    if errorCode in(1, 2): # print error information, print usage and exit
+    if errorCode in (1, 2): # print error information, print usage and exit
         print(extra)
         usage(errorCode)
     elif errorCode == 3: # print error information and exit
@@ -28,5 +28,49 @@ def usage(exitCode):
     print("\nUsage:")
     print("----------------------------------------")
     print("%s " % sys.argv[0])
-
+    
+    print("\n%s -v" % sys.argv[0])
+    print("    Verbose output")
+    
+    print("\n%s -h" % sys.argv[0])
+    print("    Show help")
+    
+    print("\nrdserialtool dps --help")
+    print("----------------------------------------")
+    runSubprocess("which rdserialtool", False)
+    print()
+    runSubprocess("rdserialtool dps --help", False)
     sys.exit(exitCode)
+    
+def runSubprocess(cmd, verbose):
+    if verbose:
+        print("\n--- Running subprocess")
+        print("    Constructing command from \n    " + cmd + " ...")
+        
+    cmdList = cmd.split()
+    
+    if verbose:
+        print("    Command list: \n        " + str(cmdList))
+                
+    #print("\n" + cmdList[0] + " session starts\n----------")
+    response = subprocess.run(cmdList)
+    #print("----------\n" + cmdList[0] + " session ended")
+    
+    returnCode = response.returncode
+    if returnCode != 0:
+        print("\nProcess exited uncleanly\nwith exit code " + str(response.returncode))
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
